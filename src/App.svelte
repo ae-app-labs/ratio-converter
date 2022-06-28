b2f152898486f51fd2afcce9c8a8c909f9eb0c13
midhunhk <2026817+midhunhk@users.noreply.github.com>
2022-06-28 15:55:08 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Converter from './lib/Converter.svelte'
+</script>
+
+<main>
+  <h1>Ratio Converter</h1>
+  
+  <p class="subtitle">
+    A handy utility for ratio conversion
+  </p>
+
+  <p>
+    Enter the reference Width and Height resolution, and enter either width or height for the target resolution. <br/>
+    Focus out of the text field and the other value is auto calculated.
+  </p>
+
+  <Converter />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #1fa591;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/lib/Converter.svelte
@@ -0,0 +1,103 @@
+<script>
+    let referenceWidth
+    let referenceHeight
+
+    let newWidth
+    let newHeight
+    
+    let errorMessage
+
+    const calculateHeight = () => {
+        if(newWidth < 1) {
+            errorMessage = "The new width has to be greater than 0"
+            return
+        }
+        if(!newWidth){
+            errorMessage = "Please enter new Width"
+            return
+        }
+        if(!referenceHeight || !referenceWidth){
+            errorMessage = "Reference values missing"
+            return
+        }
+        if(referenceWidth < 1) {
+            errorMessage = "The reference width has to be greater than 0"
+            return
+        }
+
+        let ratio = referenceHeight / referenceWidth
+        newHeight = Math.round(newWidth * ratio)
+        errorMessage = false
+    }
+
+    const calculateWidth = () => {
+        if(newHeight < 1) {
+            errorMessage = "The new height has to be greater than 0"
+            return
+        }
+        if(!newHeight){
+            errorMessage = "Please enter new Width"
+            return
+        }
+        if(!referenceHeight || !referenceWidth){
+            errorMessage = "Reference values missing"
+            return
+        }
+        if(referenceHeight < 1) {
+            errorMessage = "The reference height has to be greater than 0"
+            return
+        }
+
+        let ratio = referenceWidth/ referenceHeight
+        newWidth = Math.round(newHeight * ratio)
+        errorMessage = false
+    }
+
+</script>
+
+<div class="container">
+    <!-- Row 1 -->
+    <div class="columns is-centered is-mobile">
+        <div class="column is-one-quarter">
+            <div class="field">
+                <div class="control has-floating-label">
+                    <input class="input is-medium with-floating-label mt-2" id="referenceWidth" type="number" placeholder=" " bind:value={referenceWidth}>
+                    <label class="label is-medium is-floating-label has-text-grey" for="referenceWidth">Reference Width</label>
+                </div>
+            </div>
+        </div>
+        <div class="column is-one-quarter">
+            <div class="field">
+                <div class="control has-floating-label">
+                    <input class="input is-medium with-floating-label mt-2" type="number" id="referenceHeight" placeholder=" " bind:value="{referenceHeight}">
+                    <label class="label is-medium has-text-grey is-floating-label" for="referenceHeight">Reference Height</label>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Row 2 -->
+    <div class="columns is-centered is-mobile">
+        <div class="column is-one-quarter">
+            <div class="field">
+                <div class="control has-floating-label">
+                    <input class="input is-medium with-floating-label mt-2" type="number" id="newWidth" placeholder=" " bind:value="{newWidth}" on:blur={calculateHeight}>
+                    <label class="label is-medium has-text-grey is-floating-label" for="newWidth">New Width</label>
+                </div>
+            </div>
+        </div>
+        <div class="column is-one-quarter">
+            <div class="field">
+                <div class="control has-floating-label">
+                    <input class="input is-medium with-floating-label mt-2" type="number" id="newHeight" placeholder=" " bind:value="{newHeight}" on:blur={calculateWidth}>
+                    <label class="label is-medium has-text-grey is-floating-label" for="newHeight">New Height</label>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {#if errorMessage }
+	    <p class="p-2 tag is-danger is-medium">{errorMessage}</p>
+    {/if}
+
+</div>
